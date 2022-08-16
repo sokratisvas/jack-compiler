0f47e3c5b4c439d8c056965de7d1c54165f33672
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-08-16 18:52:28 +0300

    add keywords hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
+let keywords = HashMap::from([
+        (0, "class".to_string()),
+        (1, "constructor".to_string()),
+        (2, "function".to_string()),
+        (3, "method".to_string()),
+        (4, "field".to_string()),
+        (5, "static".to_string()),
+        (6, "var".to_string()),
+        (7, "int".to_string()),
+        (8, "char".to_string()),
+        (10, "boolean".to_string()),
+        (11, "void".to_string()),
+        (12, "true".to_string()),
+        (13, "false".to_string()),
+        (14, "null".to_string()),
+        (15, "this".to_string()),
+        (16, "let".to_string()),
+        (17, "do".to_string()),
+        (18, "if".to_string()),
+        (19, "else".to_string()),
+        (20, "while".to_string()),
+        (21, "return".to_string()),
+]);
+
 fn main() {
-    
+     
 }
