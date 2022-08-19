02146c629191744bf393ac824d62caa2959225ae
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-08-19 21:37:00 +0300

    add tokenizer

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,49 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn is_symbol(token: char) -> bool {
-    token == '{' ||
-    token == '}' ||
-    token == '[' ||
-    token == ']' ||
-    token == '(' ||
-    token == ')' ||
-    token == '.' ||
-    token == ',' ||
-    token == ';' ||
-    token == '+' ||
-    token == '-' ||
-    token == '*' ||
-    token == '/' ||
-    token == '&' ||
-    token == '|' ||
-    token == '<' ||
-    token == '>' ||
-    token == '=' ||
-    token == '~'
+    token == '{'
+        || token == '}'
+        || token == '['
+        || token == ']'
+        || token == '('
+        || token == ')'
+        || token == '.'
+        || token == ','
+        || token == ';'
+        || token == '+'
+        || token == '-'
+        || token == '*'
+        || token == '/'
+        || token == '&'
+        || token == '|'
+        || token == '<'
+        || token == '>'
+        || token == '='
+        || token == '~'
 }
 
 fn is_keyword(token: String) -> bool {
-    token == "class".to_string() ||
-    token == "constructor".to_string() ||
-    token == "function".to_string() ||
-    token == "method".to_string() ||
-    token == "field".to_string() ||
-    token == "static".to_string() ||
-    token == "var".to_string() ||
-    token == "int".to_string() ||
-    token == "char".to_string()  ||
-    token == "boolean".to_string() ||
-    token == "void".to_string() ||
-    token == "true".to_string() ||
-    token == "false".to_string() ||
-    token == "null".to_string() ||
-    token == "this".to_string() ||
-    token == "let".to_string() ||
-    token == "do".to_string() ||
-    token == "if".to_string() ||
-    token == "else".to_string() ||
-    token == "while".to_string() ||
-    token == "return".to_string()
+    token == "class".to_string()
+        || token == "constructor".to_string()
+        || token == "function".to_string()
+        || token == "method".to_string()
+        || token == "field".to_string()
+        || token == "static".to_string()
+        || token == "var".to_string()
+        || token == "int".to_string()
+        || token == "char".to_string()
+        || token == "boolean".to_string()
+        || token == "void".to_string()
+        || token == "true".to_string()
+        || token == "false".to_string()
+        || token == "null".to_string()
+        || token == "this".to_string()
+        || token == "let".to_string()
+        || token == "do".to_string()
+        || token == "if".to_string()
+        || token == "else".to_string()
+        || token == "while".to_string()
+        || token == "return".to_string()
 }
 
 fn tokenize(contents: Vec<char>) -> Vec<String> {
@@ -57,12 +57,14 @@ fn tokenize(contents: Vec<char>) -> Vec<String> {
             _ if contents[position].clone().is_alphabetic() => {
                 token.push(contents[position].clone());
                 position += 1;
-                while position != contents.len() && (contents[position].is_alphanumeric() || contents[position] == '_') {
+                while position != contents.len()
+                    && (contents[position].is_alphanumeric() || contents[position] == '_')
+                {
                     token.push(contents[position].clone());
                     position += 1;
                 }
                 tokens.push(token.clone());
-            },
+            }
             _ if contents[position].clone().is_numeric() => {
                 token.push(contents[position].clone());
                 position += 1;
@@ -71,15 +73,15 @@ fn tokenize(contents: Vec<char>) -> Vec<String> {
                     position += 1;
                 }
                 tokens.push(token.clone());
-            },
+            }
             _ if is_symbol(contents[position].clone()) && contents[position].clone() != '/' => {
                 token.push(contents[position].clone());
                 tokens.push(token.clone());
                 position += 1;
-            },
+            }
             '/' => {
                 position += 1;
-                
+
                 if position == contents.len() {
                     break;
                 }
@@ -90,17 +92,18 @@ fn tokenize(contents: Vec<char>) -> Vec<String> {
                     }
                 } else if contents[position].clone() == '*' {
                     position += 1;
-                    while contents[position].clone() != '*' || contents[position + 1].clone() != '/' {
+                    while contents[position].clone() != '*' || contents[position + 1].clone() != '/'
+                    {
                         position += 1;
                     }
                     position += 2;
                 } else {
                     break;
                 }
-            },
+            }
             '\n' | ' ' | '\t' => {
                 position += 1;
-            },
+            }
             '"' => {
                 position += 1;
                 token.push('"');
@@ -111,7 +114,7 @@ fn tokenize(contents: Vec<char>) -> Vec<String> {
                 position += 1;
                 token.push('"');
                 tokens.push(token.clone());
-            },
+            }
             _ => {
                 panic!("Invalid character!");
             }
@@ -125,7 +128,8 @@ fn main() {
     let filename = String::from("examples/prog.jack");
     let mut buf = Vec::<u8>::new();
     let mut contents: Vec<char> = Vec::new();
-    let mut file = BufReader::new(File::open(filename).expect("Something went wrong reading the file"));
+    let mut file =
+        BufReader::new(File::open(filename).expect("Something went wrong reading the file"));
 
     while file.read_until(b'\n', &mut buf).expect("read_until failed") != 0 {
         let s = String::from_utf8(buf).expect("from_utf8 failed");
